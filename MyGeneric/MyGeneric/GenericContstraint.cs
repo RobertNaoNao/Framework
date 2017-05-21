d2e6ed0c68f8dceff8ed1d0abd251a1d7ee1b2e5
zhaonaonao <zhaonaonao@beisen.com>
2017-05-21 17:40:31 +0800

    泛型的约束

---
+++ MyGeneric/MyGeneric/GenericContstraint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGeneric
+{
+    class GenericContstraint
+    {
+        public static T Get<T>(T　t) where T:class ,new (){//引用类型,  还可以new一个对象
+            return default(T);
+        }
+        public static T Query<T>(T t) where T : struct //是一个值类型
+        {
+            return default(T);
+        }
+    }
+}
